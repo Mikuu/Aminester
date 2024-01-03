c93d0867e648df974d6f838892f5cf7cfc9867ee
Biao <biaofu@thoughtworks.com>
2024-01-03 17:13:44 +0800

    updating

---
+++ src/components/FolderBuilder.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="5">
+          <v-text-field label="Storage root path" :model-value="storageRootPath"></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="5">
+          <v-text-field label="Decompress passcode file" :model-value="decompressPasscodeFile"></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="2">
+          <v-text-field label="Start index" :model-value="startIndex"></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-divider/>
+
+      <v-row class="mt-2">
+        <v-col cols="12" sm="4">
+          <v-text-field label="Object name" :model-value="objectName"></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-text-field label="Decompress passcode" :model-value="decompressPasscode"></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="1">
+          <v-btn variant="outlined" class="mt-3">Add</v-btn>
+        </v-col>
+        <v-col cols="12" sm="1">
+          <v-btn variant="outlined" class="mt-3">Create</v-btn>
+        </v-col>
+      </v-row>
+
+      <v-row class="mt-2">
+        <v-col cols="12" sm="12">
+          <v-table>
+            <thead>
+              <tr>
+                <th class="text-left">Directory name</th>
+                <th class="text-left">Decompress passcode</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in objectItems" :key="item.name">
+                <td>{{ item.name }}</td>
+                <td>{{ item.decompressPasscode }}</td>
+              </tr>
+            </tbody>
+          </v-table>
+        </v-col>
+      </v-row>
+
+    </v-container>
+  </v-form>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const storageRootPath = ref("");
+const decompressPasscodeFile = ref("");
+const startIndex = ref("");
+const objectName = ref("");
+const decompressPasscode = ref("");
+const objectItems = ref([]);
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/MainBoard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card>
+    <v-tabs
+        v-model="tab"
+        color="deep-purple-accent-4"
+        align-tabs="center"
+    >
+      <v-tab value="1">Folder builder</v-tab>
+      <v-tab value="2">Sub renaming</v-tab>
+      <v-tab value="3">Directory flatten</v-tab>
+    </v-tabs>
+    <v-window v-model="tab">
+      <v-window-item value="1" class="mt-4">
+        <FolderBuilder/>
+      </v-window-item>
+      <v-window-item value="2" class="mt-4">
+        Sub renaming
+      </v-window-item>
+      <v-window-item value="3" class="mt-4">
+        Directory flatten
+      </v-window-item>
+    </v-window>
+  </v-card>
+</template>
+
+<script setup>
+import FolderBuilder from "@/components/FolderBuilder.vue";
+import { ref } from "vue";
+
+const tab = ref(null);
+</script>
+
+<style scoped>
+
+</style>
